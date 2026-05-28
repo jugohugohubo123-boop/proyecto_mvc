e1d53ec6abc41ba316c531fa489a0f2e73acc003
Rodriguez12 <vivejuegarie@gmail.com>
2026-05-28 18:38:10 -0500

    se creo la BD nueva

--- config/conexion.php
+++ config/conexion.php
@@ -1,1 +1,17 @@
-. 
+<?php
+class Conexion {
+    public static function conectar() {
+        $host = "localhost";
+        $db = "db_proyecto_mvc"; // <--- Aquí conectamos a tu nueva base de datos
+        $user = "root";
+        $password = "";
+
+        try {
+            // Creamos la conexión usando la librería PDO de PHP
+            $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
+            return $conexion;
+        } catch (PDOException $e) {
+            die("Error en la conexión a la base de datos: " . $e->getMessage());
+        }
+    }
+}
